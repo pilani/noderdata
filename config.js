5387c12e7c524f3548e462c4a36cd3a92704e73b
pilani <rbpilani@gmail.com>
2012-12-11 16:00:35 +0530

    few changes related to logging and catching invalid key value exception

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 var map = new Object();
 map["MIS"]="MIS"; 
 map["FILE_ROLLOVER_TIME"]=5*60*1000;	 	
-map["BASE_DATA_PATH"]="/home/pradeep/code/node/rdata-consumer-node/files/";
+map["BASE_DATA_PATH"]="/home/pradeep/.rdata/rfiles/";
 map["SRV_FILE_PREFIX"]="1";// should be read outside the server
 map["BASE_GSSTORAGE_BUCKET"]="rb-test-bucket/";
 map["MIS_TABLE"]="rbdata.prodtabledummy";
-map["GSUPLOADED_DATA_PATH"]="/home/pradeep/code/node/rdata-consumer-node/files/gsuploaded/";
-map["BQFAILED_DATA_PATH"]="/home/pradeep/code/node/rdata-consumer-node/files/bqfailed/";
-map["BQIMPORTED_DATA_PATH"]="/home/pradeep/code/node/rdata-consumer-node/files/bqimported/";
+map["GSUPLOADED_DATA_PATH"]=map["BASE_DATA_PATH"]+"gsuploaded/";
+map["BQFAILED_DATA_PATH"]=map["BASE_DATA_PATH"]+"bqfailed/";
+map["BQIMPORTED_DATA_PATH"]=map["BASE_DATA_PATH"]+"bqimported/";
 map["RABBIT-HOST"]="10.120.10.33";
 map["QS"]=["MIS","MIS_API","LIS","LIS_API"];
 
