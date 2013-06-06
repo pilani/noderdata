ab846688d519049143156b34d53f63c4323e7c8c
Ubuntu <ubuntu@ip-10-134-231-240.ap-southeast-1.compute.internal>
2013-06-06 12:06:07 +0530

    Bug fix

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ var bqfilepath=cfg.getFileName(deliveryinfo.queue);
 
 //var s3filepath=cfg.config["BASE_DATA_PATH"]+deliveryinfo.queue+".map";
 try{
-     var kvmap = bqutil.formMapFromStringV2(msg,keys.keys);
+     var kvmap = bqutil.formMapFromString(msg,keys.keys);
      var val = bqutil.formBqCompliantLine(kvmap,columns.columns,ckmap.ckmap,ctypes)
      //messlog.info(val);  -- this is a big perf impact
      fs.appendToFile(bqfilepath,val+"\n",errHandler);
